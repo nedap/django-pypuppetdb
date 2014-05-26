f48dbc4484f24f361626530addf33a474a1ad401
Ronald van Zon <ronald.vanZon@nedap.com>
2014-05-26 15:08:20 +0200

    Update test settings

--- conftest.py
+++ conftest.py
@@ -15,5 +15,13 @@ def pytest_configure():
         CACHES={'default': {
             'BACKEND': 'django.core.cache.backends.dummy.DummyCache'}},
         INSTALLED_APPS=('django_bootstrap_breadcrumbs',),
-        ROOT_URLCONF='tests.urls'
+        ROOT_URLCONF='tests.urls',
+
+        # Puppetdb config settings
+        PUPPETDB_HOST = 'localhost',
+        PUPPETDB_PORT = 8080,
+        PUPPETDB_NODE = 'node',
+        PUPPETDB_KEY  = None,
+        PUPPETDB_CERT = None,
+        PUPPETDB_SSL_VERIFY = False
     )
