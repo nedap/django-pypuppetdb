93b36e322cc337e79d8edc9e49411398aab9c923
Ronald van Zon <ronald.vanZon@nedap.com>
2014-05-28 09:53:33 +0200

    Tests for django authentication

---
+++ tests/test_django_authentication.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from mock import patch
+from django_pypuppetdb.django_authentication import PuppetDbAuthentication
+
+
+class TestAccountModel(TestCase):
+    def setUp(self):
+        self.user = User(username='test', email='test@nedap.com')
+        self.auth = PuppetDbAuthentication()
+
+    def test_authenticate_without_user(self):
+        self.assertIsNone(self.auth.authenticate())
+
+    @patch(
+        'django_pypuppetdb.user_authentication.UserAuthentication.check_user')
+    @patch('django_pypuppetdb.user_authentication.UserAuthentication.'
+           'verify_password')
+    def test_authenticate_incorrect_password(
+            self, puppetdb_user, verify_password):
+        puppetdb_user.return_value = None
+        verify_password.return_value = True
+        self.assertIsNone(self.auth.authenticate('test'))
+
+    @patch(
+        'django_pypuppetdb.user_authentication.UserAuthentication.check_user')
+    @patch('django_pypuppetdb.user_authentication.UserAuthentication.'
+           'verify_password')
+    def test_authenticate_with_existing_user(
+            self, puppetdb_user, verify_password):
+        puppetdb_user.return_value = 'test'
+        verify_password.return_value = True
+        user = self.auth.authenticate('test', 'password')
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, 'test')
+
+    @patch(
+        'django_pypuppetdb.user_authentication.UserAuthentication.check_user')
+    @patch('django_pypuppetdb.user_authentication.UserAuthentication.'
+           'verify_password')
+    def test_authenticate_with_new_user(self, puppetdb_user, verify_password):
+        puppetdb_user.return_value = 'new user'
+        verify_password.return_value = True
+        user = self.auth.authenticate('new user', 'password')
+        self.assertIsInstance(user, User)
+        self.assertEqual(user.username, 'new user')
+        self.assertIsNotNone(user.api_key)
+
+    def test_get_user_with_incorrect_id(self):
+        self.assertIsNone(self.auth.get_user(2))
+
+    def test_get_user(self):
+        user = self.auth.get_user(self.user.id)
+        self.assertEqual(user, self.user)
