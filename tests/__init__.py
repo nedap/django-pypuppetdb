d7ea834a74c0518925059fa7140efe4739b3c9ba
Ronald van Zon <ronald.vanZon@nedap.com>
2014-06-02 11:43:06 +0200

    Improved tests for django authentication

--- tests/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-:copyright: Copyright 2014 by Ronald van Zon
-:contact: rvzon84+pypuppetdb@gmail.com
-"""
--- tests/test_django_authentication.py
+++ tests/test_django_authentication.py
@@ -1,3 +1,4 @@
+import logging
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.runner import setup_databases
@@ -15,17 +16,13 @@ class TestDjangoAuthentication(TestCase):
             username='test', email='test@nedap.com')
         self.auth = PuppetDBAuthentication()
 
-    def test_authenticate_without_user(self):
+    def test_authenticate_without_connection(self):
         self.assertIsNone(self.auth.authenticate())
 
     @patch(
         'django_pypuppetdb.user_authentication.UserAuthentication.check_user')
-    @patch('django_pypuppetdb.user_authentication.UserAuthentication.'
-           'verify_password')
-    def test_authenticate_incorrect_password(
-            self, puppetdb_user, verify_password):
-        puppetdb_user.return_value = None
-        verify_password.return_value = True
+    def test_authenticate_incorrect_username(self, check_user):
+        check_user.return_value = None
         self.assertIsNone(self.auth.authenticate('test'))
 
     @patch(
@@ -33,8 +30,8 @@ class TestDjangoAuthentication(TestCase):
     @patch('django_pypuppetdb.user_authentication.UserAuthentication.'
            'verify_password')
     def test_authenticate_with_existing_user(
-            self, puppetdb_user, verify_password):
-        puppetdb_user.return_value = 'test'
+            self, check_user, verify_password):
+        check_user.return_value = 'test'
         verify_password.return_value = True
         user = self.auth.authenticate('test', 'password')
         self.assertIsInstance(user, User)
@@ -44,8 +41,8 @@ class TestDjangoAuthentication(TestCase):
         'django_pypuppetdb.user_authentication.UserAuthentication.check_user')
     @patch('django_pypuppetdb.user_authentication.UserAuthentication.'
            'verify_password')
-    def test_authenticate_with_new_user(self, puppetdb_user, verify_password):
-        puppetdb_user.return_value = 'new user'
+    def test_authenticate_with_new_user(self, check_user, verify_password):
+        check_user.return_value = 'new user'
         verify_password.return_value = True
         user = self.auth.authenticate('new user', 'password')
         self.assertIsInstance(user, User)
